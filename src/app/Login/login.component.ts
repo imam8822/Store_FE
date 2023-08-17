6059f404e75878413a1f5741a8ac037c1b7b1f24
User <User@DESKTOP-1VGFKB9>
2023-08-17 16:29:44 +0530

    Registration Completed

--- src/app/Login/login.component.ts
+++ src/app/Login/login.component.ts
@@ -1,63 +1,63 @@
 import { Component } from '@angular/core';
-import { FormControl,FormGroup, Validators } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { NgForm } from '@angular/forms';
 import { AuthenticationService } from '../services/authentication.service';
 import { Login } from '../Models/login';
 import { Token } from '@angular/compiler';
 import { Response } from '../Models/response';
 import { ToastrService } from 'ngx-toastr';
-import { Router } from '@angular/router'; 
+import { Router } from '@angular/router';
 import { user } from '../Models/user';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
-
-
 export class LoginComponent {
-
-constructor(private authService:AuthenticationService,private toast:ToastrService,private router:Router){}
-
+  constructor(
+    private authService: AuthenticationService,
+    private toast: ToastrService,
+    private router: Router
+  ) {}
+  showLoading: boolean = false;
   loginDTO = new Login();
   response = new Response();
   loginForm = new FormGroup({
-  email : new FormControl("email",[Validators.required,Validators.email]),
-  password : new FormControl("password",[Validators.required,Validators.minLength(6)])
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(6),
+    ]),
   });
 
-  userDTO = new user()
+  userDTO = new user();
 
-  get Email (): FormControl {
-    return this.loginForm.get("email") as FormControl;
+  get Email(): FormControl {
+    return this.loginForm.get('email') as FormControl;
   }
 
-  get Pwd (): FormControl {
-    return this.loginForm.get("password") as FormControl;
+  get Pwd(): FormControl {
+    return this.loginForm.get('password') as FormControl;
   }
 
-
-  Login(){
-    this.loginDTO.email = this.loginForm.value.email!;
-    this.loginDTO.password = this.loginForm.value.password!;
-
-    this.authService.login(this.loginDTO).subscribe((response)=>{
-      if(response.isSuccess)
-      {
-          this.toast.success("Login Successful")
-          debugger;
+  Login() {
+    if (this.loginForm.valid) {
+      this.showLoading = true;
+      this.loginDTO.email = this.loginForm.value.email!;
+      this.loginDTO.password = this.loginForm.value.password!;
+      this.authService.login(this.loginDTO).subscribe((response) => {
+        if (response.isSuccess) {
+          this.toast.success('Login Successful');
           this.userDTO = JSON.parse(response.message);
-          localStorage.setItem("Token",this.userDTO.Token);
-          localStorage.setItem("Username",this.userDTO.FirstName);
+          localStorage.setItem('Token', this.userDTO.Token);
+          localStorage.setItem('Username', this.userDTO.FirstName);
           this.authService.isLoggedIn.next(true);
-          this.router.navigateByUrl("home")
-      }
-      else
-      {
-        this.toast.error("Invalid User")
-      }
-    })
+          this.router.navigateByUrl('home');
+        } else {
+          this.toast.error('Invalid User');
+        }
+      });
+      this.showLoading = false;
+    }
   }
 }
-
-
