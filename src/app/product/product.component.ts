6b934daf0fa20c5e6d627ef87f67f068f3284e69
User <User@DESKTOP-1VGFKB9>
2023-08-21 15:50:44 +0530

    Jquery table implemented

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../services/Product/product.service';
 import { Product } from '../Models/product';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-product',
@@ -9,10 +10,25 @@ import { Product } from '../Models/product';
 })
 export class ProductComponent implements OnInit {
   products: Product[] = [];
+  dtOptions: DataTables.Settings = {};
+dtTrigger : Subject<any> = new Subject<any>();
   constructor(private _service: ProductService) { }
   ngOnInit(): void {
+
+    this.dtOptions = {
+      pagingType: 'full_numbers',
+      pageLength: 5,
+      processing: true
+    };
+
+    this.LoadProducts();
+
+  }
+
+  LoadProducts(){
     this._service.getAllProducts().subscribe((res) => {
       this.products = res.data;
+      this.dtTrigger.next(null);
     });
   }
 
